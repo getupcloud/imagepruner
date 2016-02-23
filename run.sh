743870f00a752167a456adf37e445a7ba918f8db
Diego Castro (dscastro) <spinolacastro@gmail.com>
2016-02-23 11:25:35 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-exec "$@"
+oc adm prune deployments --orphans --keep-complete=5 --keep-failed=1 --keep-younger-than=24h --confirm
+
+oc adm prune builds --orphans --keep-complete=5 --keep-failed=1 --keep-younger-than=24h --confirm
+
+oc adm prune images --keep-tag-revisions=5 --keep-younger-than=24h --confirm
