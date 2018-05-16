453468eb3a5cdd6a004d4e5db6b5151cf4b266a7
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2018-05-16 07:31:09 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if oc get serviceaccount image-spruner -n default -o name; then
+if oc get serviceaccount image-pruner -n default -o name; then
   oc create serviceaccount image-pruner -n default
   oadm policy add-cluster-role-to-user cluster-admin system:serviceaccount:default:image-pruner
 fi
